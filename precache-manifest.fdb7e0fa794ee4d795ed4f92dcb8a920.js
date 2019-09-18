8349538eac661b4f27e65acde9f3592012266bcb
subhom11 <subhomkundu@gmail.com>
2019-09-18 23:30:47 +0530

    Updates

---
+++ precache-manifest.fdb7e0fa794ee4d795ed4f92dcb8a920.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529e638860d314f5b0fcb41bbdeceec7",
+    "url": "/react-state-management/index.html"
+  },
+  {
+    "revision": "765c08fb252d847fa4bb",
+    "url": "/react-state-management/static/css/main.1a85fe63.chunk.css"
+  },
+  {
+    "revision": "3eb7bbc68e7ebc4ff4a9",
+    "url": "/react-state-management/static/js/2.a06c541b.chunk.js"
+  },
+  {
+    "revision": "765c08fb252d847fa4bb",
+    "url": "/react-state-management/static/js/main.c2a278a7.chunk.js"
+  },
+  {
+    "revision": "e3a3c4a6693cea56b095",
+    "url": "/react-state-management/static/js/runtime~main.49b3523b.js"
+  }
+]);
